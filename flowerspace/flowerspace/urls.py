e8f8b1952e716eddaaa004bf15152abb522d3a8c
phony <whyamistillalive79@gmail.com>
2025-02-12 18:38:07 +0330

    changed admin panel url

--- flowerspace/flowerspace/urls.py
+++ flowerspace/flowerspace/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('superdupersecretadminpanel/', admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('home.urls', namespace='home')),
